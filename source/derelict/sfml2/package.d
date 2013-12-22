eed684ca536cbdb66aa85916315865f54ac85155
Mike Parker <aldacron@gmail.com>
2013-12-22 18:38:04 +0900

    Removed package.d

--- source/derelict/sfml2/package.d
+++
@@ -1,7 +0,0 @@
-module derelict.sfml2;
-
-public import derelict.sfml2.audio,
-              derelict.sfml2.graphics,
-              derelict.sfml2.network,
-              derelict.sfml2.system,
-              derelict.sfml2.window;
